3f0f1e1facdfa6bce0edd2be1362ca109da2bbf3
Kitvald <makosov.vald123@mail.ru>
2025-07-23 22:32:52 +0300

    Add files via upload

---
+++ zadacha.py
@@ -0,0 +1,28 @@
+def max_number(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+def even_numbers(n):
+    for i in range(0, n+1, 2):
+        yield i
+
+
+def empty_function():
+    pass
+
+
+a = int(input("введите число первое: "))
+b = int(input("введите число второе: "))
+number = max_number(a,b)
+print(number)
+
+pass_function = empty_function()
+print(pass_function)
+
+n = int(input("введите число : "))
+gen = even_numbers(n)
+print(list(gen))
+
