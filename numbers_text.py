45aa34e87905ba727c0d0a9ffafe364546de40ff
Kitvald <makosov.vald123@mail.ru>
2025-08-28 17:15:52 +0300

    Add files via upload

---
+++ numbers_text.py
@@ -0,0 +1,14 @@
+number = int(input("Введите цифру от 1 до 5: "))
+
+if number == 1:
+    print("one")
+elif number == 2:
+    print("two")
+elif number == 3:
+    print("three")
+elif number == 4:
+    print("four")
+elif number == 5:
+    print("five")
+else:
+    print("Введенная цифра не соответствует заданному диапазону")
