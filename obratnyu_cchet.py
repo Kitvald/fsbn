ba55fdf6300eb70a396e9d6bac38b65521746397
Kitvald <makosov.vald123@mail.ru>
2025-09-01 20:40:43 +0300

    Add files via upload

--- obratnyu_cchet.py
+++ obratnyu_cchet.py
@@ -1,4 +1,8 @@
-number = abs(int(input("Введите ваше число: ")))
+try:
+    number = abs(int(input("Введите ваше число: ")))
+except ValueError:
+    print("Ошибка! Введите целое число, а не текст.")
 while number > -1:
     print("Счетчик =", number)
     number -= 1
+    
