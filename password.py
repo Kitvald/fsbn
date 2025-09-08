f9a7c608c40aaa1f5f20bfdd780fa30992d7dc9c
Kitvald <makosov.vald123@mail.ru>
2025-09-08 19:28:46 +0300

    Add files via upload

--- password.py
+++ password.py
@@ -1,9 +1,7 @@
 a = input("Введите ваш пароль: ")
 password = "qwerty"
 
-while a != password:
-    if a != password:
-        a = input("Введите ваш пароль: ")
-else:
-    print("вы ввели тот самый пароль, перепарольте")
-    a = input("Введите ваш пароль: ")
+while a == password:
+    if a == password:
+        print("Пароль принят")
+    break
