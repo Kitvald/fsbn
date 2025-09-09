0e0eb60b8dc6c22dcf00fee80585964816806e62
Kitvald <makosov.vald123@mail.ru>
2025-09-09 16:21:12 +0300

    Add files via upload

--- password.py
+++ password.py
@@ -1,7 +1,10 @@
-a = input("Введите ваш пароль: ")
-password = "qwerty"
-
-while a == password:
+while True:
+    a = input("Введите ваш пароль: ")
+    password = "qwerty"
     if a == password:
-        print("Пароль принят")
-    break
+        print("Пароль введен верно")
+        break
+    else:
+        a != password
+        print("Пароль введен не верно")
+        # следует ли добавить break после введения неправильного пароля
