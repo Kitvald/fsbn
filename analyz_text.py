7907497b23d4080148282838995f684fe1158828
Kitvald <makosov.vald123@mail.ru>
2025-09-18 19:45:57 +0300

    Add files via upload

--- analyz_text.py
+++ analyz_text.py
@@ -1,11 +1,22 @@
-from dataclasses import replace
-
-my_string = "Делать чёрным, придавать чёрный  цвет, цвет цвет оттенок." # для теста функции for
-my_string_clean = my_string.replace(',', '').replace('.', '')
+my_string = "Делать делать чёрным, Ппридавать чёрный  цвет, цвет цвет оттенок." # для теста функции for
+my_string_clean = my_string.replace(',', '').replace('.', '').lower()
 words = my_string_clean.split()
+dict_word = {}
+glasnue = ("уёеыаоэяию")
+counts = 0
 
-for s in my_string:
-    print(max(words, key=len))
-    print(len(words))
-    #print(list(enumerate(words)))
-    break
+
+for word in words:
+    dict_word[word] = dict_word.get(word, 0) + 1
+for word, count in sorted(dict_word.items()):
+        print(word, count)
+
+
+for word in my_string_clean:
+    if word in glasnue:
+        counts += 1
+
+
+print("Количество гласных:",counts)
+print("Количество слов:",len(words))
+print("Самое длинное слово:",max(words, key=len))
