330b9483de8fe4b98fcd0abae09b7b7b9e301247
Kitvald <makosov.vald123@mail.ru>
2025-10-31 18:23:58 +0300

    Add files via upload

--- analyz_text.py
+++ analyz_text.py
@@ -1,22 +1,26 @@
-my_string = "Делать делать чёрным, Ппридавать чёрный  цвет, цвет цвет оттенок." # для теста функции for
+def create_word_dict(words):
+    dict_word = {}
+    for word in words:
+        dict_word[word] = dict_word.get(word, 0) + 1
+    return dict_word
+
+
+my_string = str(input("Введите текст: "))
 my_string_clean = my_string.replace(',', '').replace('.', '').lower()
 words = my_string_clean.split()
-dict_word = {}
-glasnue = ("уёеыаоэяию")
-counts = 0
+word_dict = create_word_dict(words)
 
+def vowels_count(text):
+    vowels = "уёеыаоэяию"
+    counts = 0
+    for char in text:
+        if char in vowels:
+            counts += 1
+    return counts
 
-for word in words:
-    dict_word[word] = dict_word.get(word, 0) + 1
-for word, count in sorted(dict_word.items()):
-        print(word, count)
+for word, count in sorted(word_dict.items()):
+    print(word, count)
 
-
-for word in my_string_clean:
-    if word in glasnue:
-        counts += 1
-
-
-print("Количество гласных:",counts)
+print("Количество гласных:",vowels_count(my_string_clean))
 print("Количество слов:",len(words))
 print("Самое длинное слово:",max(words, key=len))
