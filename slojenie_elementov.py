7c41d4560c59dce2042751eb311b76325ebe83d4
Kitvald <makosov.vald123@mail.ru>
2025-11-11 13:52:16 +0300

    Add files via upload

---
+++ slojenie_elementov.py
@@ -0,0 +1,10 @@
+data = [1, 2, 5, 6, 5, 5, 5]
+data1 = [2, 4, 5]
+
+result = []
+for i in range(max(len(data), len(data1))):
+    a = data[i] if i < len(data) else 0
+    b = data1[i] if i < len(data1) else 0
+    result.append(a + b)
+
+print(result)
