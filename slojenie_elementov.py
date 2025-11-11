0b4a91157e3ba546e2efbc30f41cbd1436b4296f
Kitvald <makosov.vald123@mail.ru>
2025-11-11 17:59:55 +0300

    Add files via upload

--- slojenie_elementov.py
+++ slojenie_elementov.py
@@ -1,12 +1,11 @@
 def adding_lists():
-    result = []
-    data = [1, 2, 5, 6, 5, 5, 5]
-    data1 = [2, 4, 5]
     for i in range(max(len(data), len(data1))):
         a = data[i] if i < len(data) else 0
         b = data1[i] if i < len(data1) else 0
         result.append(a + b)
     return result
 
-
+result = []
+data = [1, 2, 5, 6, 5, 5, 5]
+data1 = [2, 4, 5]
 print(adding_lists())
