d9f08ef86bcb0416f6404fa88339394a91422943
Kitvald <makosov.vald123@mail.ru>
2026-08-12 17:13:08 +0300

    Add files via upload

--- Average_grades.py
+++ Average_grades.py
@@ -1,28 +1,29 @@
 def calculate_average(grades):
-    avg_stud = []
     for student in grades:
         average = sum(student["grades"]) / len(student["grades"])
-        avg_stud.append(average)
-    return avg_stud
+        student["average"] = average
+    return
 
 
-def main(students, avg_stud):
-    min_grade = min(avg_stud)
-    worst_index = avg_stud.index(min_grade)
-    students[worst_index] = {"name": "Гендальф" , "grades": [100, 100, 100]}
+def main(average):
+    worst_student = min(students, key=lambda students: students["average"])
+    students.pop(students.index(worst_student))
+    students.append({"name": "Gendalf", "grades": [99, 100, 98]})
+    return calculate_average(students)
+
+
 
 students = [
     {"name": "Frodo", "grades": [87, 48, 98]}, # 77.66
     {"name": "Cem", "grades": [93, 64, 45]}, # 67,33
     {"name": "Pipin", "grades": [70, 65, 90]}, #75
 ] #73.33 общий ср балл
-
-avg_stud = calculate_average(students)
-print("старые баллы", avg_stud)
-
-main(students, avg_stud)
-new_avg_stud = calculate_average(students)
-print("новые баллы", new_avg_stud)
+calculate_average(students)
+main(students)
+print(students)
+#main(students, avg_stud)
+#new_avg_stud = calculate_average(students)
+#print("новые баллы", new_avg_stud)
 
 
 
